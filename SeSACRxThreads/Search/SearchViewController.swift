394d83ad317159187b2fdaacbbccda9dbe47ccb9
seongmin kim <k2417000@naver.com>
2024-08-01 18:06:04 +0900

    실시간 검색 기능 구현

--- SeSACRxThreads/Search/SearchViewController.swift
+++ SeSACRxThreads/Search/SearchViewController.swift
@@ -8,28 +8,28 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import SnapKit
 
 class SearchViewController: UIViewController {
-   
+    
     private let tableView: UITableView = {
-       let view = UITableView()
+        let view = UITableView()
         view.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
         view.backgroundColor = .white
         view.rowHeight = 180
         view.separatorStyle = .none
-       return view
-     }()
+        return view
+    }()
     
     let searchBar = UISearchBar()
-       
+    
     let disposeBag = DisposeBag()
     
-    var list = Observable.just(["A", "B", "C", "AB", "D", "ABC", "BBB", "EC", "SA", "AAAB", "ED", "F", "G", "H"])
+    let data = ["A", "B", "C", "AB", "D", "ABC", "BBB", "EC", "SA", "AAAB", "ED", "F", "G", "H"]
+    lazy var list = BehaviorRelay(value: data)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .white
         configure()
         setSearchController()
         bind()
@@ -38,32 +38,41 @@ class SearchViewController: UIViewController {
     func bind() {
         list
             .bind(to: tableView.rx.items(cellIdentifier: SearchTableViewCell.identifier, cellType: SearchTableViewCell.self)) { (row, element, cell) in
-                 
+                
                 cell.appNameLabel.text = element
                 cell.appIconImageView.backgroundColor = .systemBlue
-
+                
             }
             .disposed(by: disposeBag)
-         
+        
+        searchBar.rx.text.orEmpty
+            // 과호출 막기
+            .debounce(.seconds(1), scheduler: MainScheduler.instance)
+            // 중복 단어 막기
+            .distinctUntilChanged()
+            .bind(with: self) { owner, value in
+                print("검색:", value)
+                let result = value.isEmpty ? owner.data : owner.data.filter { $0.contains(value) }
+                owner.list.accept(result)
+            }
+            .disposed(by: disposeBag)
     }
-     
+    
     private func setSearchController() {
         view.addSubview(searchBar)
         navigationItem.titleView = searchBar
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(plusButtonClicked))
     }
     
     @objc func plusButtonClicked() {
         print("추가 버튼 클릭")
     }
-
     
     private func configure() {
+        view.backgroundColor = .white
         view.addSubview(tableView)
         tableView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide)
         }
-
     }
 }
