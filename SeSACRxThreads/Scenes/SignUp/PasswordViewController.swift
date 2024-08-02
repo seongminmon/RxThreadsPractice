277a0dcd867e5088aee8b421a8b7a7cfa8c44f7a
seongmin kim <k2417000@naver.com>
2024-08-02 12:01:38 +0900

    회원가입 - 패스워드 화면 구현

--- SeSACRxThreads/Scenes/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/Scenes/SignUp/PasswordViewController.swift
@@ -6,29 +6,54 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 import SnapKit
 
 class PasswordViewController: UIViewController {
    
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    let descriptionLabel = UILabel()
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = Color.white
+        configureView()
+        bind()
+    }
+    
+    func bind() {
+        let isValid = passwordTextField.rx.text.orEmpty
+            .map { $0.count >= 8 }
         
-        configureLayout()
-         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        isValid
+            .bind(with: self) { owner, value in
+                owner.nextButton.backgroundColor = value ? Color.green : Color.red
+            }
+            .disposed(by: disposeBag)
+        
+        isValid
+            .bind(to: nextButton.rx.isEnabled,
+                  descriptionLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                let vc = PhoneViewController()
+                owner.navigationController?.pushViewController(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
     }
     
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(PhoneViewController(), animated: true)
-    }
-    
-    func configureLayout() {
+    func configureView() {
+        view.backgroundColor = Color.white
+        descriptionLabel.text = "8자 이상 입력해주세요"
+        descriptionLabel.textColor = Color.red
+        
         view.addSubview(passwordTextField)
+        view.addSubview(descriptionLabel)
         view.addSubview(nextButton)
          
         passwordTextField.snp.makeConstraints { make in
@@ -37,11 +62,16 @@ class PasswordViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
         
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom)
+            make.horizontalEdges.equalTo(passwordTextField)
+            make.bottom.equalTo(nextButton.snp.top)
+        }
+        
         nextButton.snp.makeConstraints { make in
             make.height.equalTo(50)
             make.top.equalTo(passwordTextField.snp.bottom).offset(30)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
-
 }
