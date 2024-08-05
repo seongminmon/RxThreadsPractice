a5817260e78b398bf9766aa81b1a76b9c60319df
seongmin kim <k2417000@naver.com>
2024-08-05 14:31:01 +0900

    닉네임 설정 화면 MVVM 적용하기

--- SeSACRxThreads/Scenes/SignUpOptional/Nickname/NicknameViewController.swift
+++ SeSACRxThreads/Scenes/SignUpOptional/Nickname/NicknameViewController.swift
@@ -16,9 +16,8 @@ class NicknameViewController: UIViewController {
     let nextButton = PointButton(title: "다음")
     let descriptionLabel = UILabel()
     
+    let viewModel = NicknameViewModel()
     let disposeBag = DisposeBag()
-    
-    let descriptionText = PublishSubject<String>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,75 +26,30 @@ class NicknameViewController: UIViewController {
     }
     
     func bind() {
-        nicknameTextField.rx.text.orEmpty
+        let input = NicknameViewModel.Input(
+            text: nicknameTextField.rx.text.orEmpty, 
+            tap: nextButton.rx.tap
+        )
+        let output = viewModel.transform(input: input)
+        
+        output.validation
             .bind(with: self) { owner, value in
-                do {
-                    _ = try owner.checkNickname(value)
-                    owner.descriptionLabel.textColor = Color.green
-                    owner.descriptionText.onNext("사용 가능한 닉네임이에요")
-                    owner.nextButton.isEnabled = true
-                    owner.nextButton.backgroundColor = Color.green
-                } catch {
-                    guard let error = error as? NicknameValidationError else {
-                        owner.descriptionText.onNext("알 수 없는 에러가 발생했습니다")
-                        return
-                    }
-                    owner.descriptionLabel.textColor = Color.red
-                    owner.descriptionText.onNext(error.errorDescription ?? "알 수 없는 에러가 발생했습니다")
-                    owner.nextButton.isEnabled = false
-                    owner.nextButton.backgroundColor = Color.red
-                }
+                owner.nextButton.isEnabled = value
+                owner.nextButton.backgroundColor = value ? Color.green : Color.red
+                owner.descriptionLabel.textColor = value ? Color.green : Color.red
             }
             .disposed(by: disposeBag)
         
-        // 구독 시점 미뤄서 처음에는 빈 텍스트로 표시하기
-        descriptionText
+        output.description
             .bind(to: descriptionLabel.rx.text)
             .disposed(by: disposeBag)
         
-        nextButton.rx.tap
+        output.tap
             .bind(with: self) { owner, _ in
                 let vc = BirthdayViewController()
                 owner.navigationController?.pushViewController(vc, animated: true)
             }
             .disposed(by: disposeBag)
-    }
-    
-    enum NicknameValidationError: Error, LocalizedError {
-        case length
-        case invalidCharacter
-        case number
-        case whitespace
-        
-        var errorDescription: String? {
-            switch self {
-            case .length: "2글자 이상 10글자 미만으로 설정해주세요"
-            case .invalidCharacter: "닉네임에 @, #, $, % 는 포함할 수 없어요"
-            case .number: "닉네임에 숫자는 포함할 수 없어요"
-            case .whitespace: "닉네임에 공백은 포함할 수 없어요"
-            }
-        }
-    }
-    
-    func checkNickname(_ text: String) throws -> Bool {
-        // 1) 2글자 이상 10글자 미만
-        guard text.count >= 2 && text.count < 10 else {
-            throw NicknameValidationError.length
-        }
-        // 2) @, #, $, % 사용 불가
-        let invalidCharacters = "@#$%"
-        guard text.filter({ invalidCharacters.contains($0) }).isEmpty else {
-            throw NicknameValidationError.invalidCharacter
-        }
-        // 3) 숫자 사용 불가
-        guard text.filter({ $0.isNumber }).isEmpty else {
-            throw NicknameValidationError.number
-        }
-        // 4) 공백 사용 불가
-        guard text.filter({ $0.isWhitespace }).isEmpty else {
-            throw NicknameValidationError.whitespace
-        }
-        return true
     }
     
     func configureView() {
--- SeSACRxThreads/Scenes/SignUpOptional/Nickname/NicknameViewModel.swift
+++ SeSACRxThreads/Scenes/SignUpOptional/Nickname/NicknameViewModel.swift
@@ -11,15 +11,83 @@ import RxCocoa
 
 class NicknameViewModel {
     
+    let disposeBag = DisposeBag()
+    
     struct Input {
-        
+        let text: ControlProperty<String>
+        let tap: ControlEvent<Void>
     }
     
     struct Output {
-        
+        let validation: PublishSubject<Bool>
+        let description: PublishSubject<String>
+        let tap: ControlEvent<Void>
     }
     
     func transform(input: Input) -> Output {
-        return Output()
+        
+        let validation = PublishSubject<Bool>()
+        let description = PublishSubject<String>()
+        
+        input.text
+            .bind(with: self) { owner, value in
+                do {
+                    _ = try owner.checkNickname(value)
+                    validation.onNext(true)
+                    description.onNext("사용 가능한 닉네임이에요")
+                } catch {
+                    validation.onNext(false)
+                    
+                    guard let error = error as? NicknameValidationError, let errorDescription = error.errorDescription else {
+                        description.onNext("알 수 없는 에러가 발생했습니다")
+                        return
+                    }
+                    description.onNext(errorDescription)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(
+            validation: validation,
+            description: description,
+            tap: input.tap
+        )
+    }
+    
+    enum NicknameValidationError: Error, LocalizedError {
+        case length
+        case invalidCharacter
+        case number
+        case whitespace
+        
+        var errorDescription: String? {
+            switch self {
+            case .length: "2글자 이상 10글자 미만으로 설정해주세요"
+            case .invalidCharacter: "닉네임에 @, #, $, % 는 포함할 수 없어요"
+            case .number: "닉네임에 숫자는 포함할 수 없어요"
+            case .whitespace: "닉네임에 공백은 포함할 수 없어요"
+            }
+        }
+    }
+    
+    func checkNickname(_ text: String) throws -> Bool {
+        // 1) 2글자 이상 10글자 미만
+        guard text.count >= 2 && text.count < 10 else {
+            throw NicknameValidationError.length
+        }
+        // 2) @, #, $, % 사용 불가
+        let invalidCharacters = "@#$%"
+        guard text.filter({ invalidCharacters.contains($0) }).isEmpty else {
+            throw NicknameValidationError.invalidCharacter
+        }
+        // 3) 숫자 사용 불가
+        guard text.filter({ $0.isNumber }).isEmpty else {
+            throw NicknameValidationError.number
+        }
+        // 4) 공백 사용 불가
+        guard text.filter({ $0.isWhitespace }).isEmpty else {
+            throw NicknameValidationError.whitespace
+        }
+        return true
     }
 }
