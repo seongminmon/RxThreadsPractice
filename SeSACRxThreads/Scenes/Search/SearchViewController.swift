debcced234cd815cfc0fa4c5d8b079a88702e5f7
seongmin kim <k2417000@naver.com>
2024-08-05 21:29:21 +0900

    검색 화면 MVVM 적용

--- SeSACRxThreads/Scenes/Search/SearchViewController.swift
+++ SeSACRxThreads/Scenes/Search/SearchViewController.swift
@@ -23,10 +23,8 @@ class SearchViewController: UIViewController {
     
     let searchBar = UISearchBar()
     
+    let viewModel = SearchViewModel()
     let disposeBag = DisposeBag()
-    
-    var data = ["A", "B", "C", "AB", "D", "ABC", "BBB", "EC", "SA", "AAAB", "ED", "F", "G", "H"]
-    lazy var list = BehaviorRelay(value: data)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,8 +34,17 @@ class SearchViewController: UIViewController {
     }
     
     func bind() {
-        list
-            .bind(to: tableView.rx.items(cellIdentifier: SearchTableViewCell.identifier, cellType: SearchTableViewCell.self)) { (row, element, cell) in
+        let input = SearchViewModel.Input(
+            searchText: searchBar.rx.text.orEmpty,
+            barButtonTap: navigationItem.rightBarButtonItem?.rx.tap
+        )
+        let output = viewModel.transform(input: input)
+        
+        output.list
+            .bind(to: tableView.rx.items(
+                cellIdentifier: SearchTableViewCell.identifier,
+                cellType: SearchTableViewCell.self)
+            ) { (row, element, cell) in
                 cell.appNameLabel.text = element
                 cell.appIconImageView.backgroundColor = .systemBlue
                 cell.downloadButton.rx.tap
@@ -50,39 +57,13 @@ class SearchViewController: UIViewController {
                     .disposed(by: cell.disposeBag)
             }
             .disposed(by: disposeBag)
-        
-        searchBar.rx.text.orEmpty
-            // 과호출 막기
-            .debounce(.seconds(1), scheduler: MainScheduler.instance)
-            // 중복 단어 막기
-            .distinctUntilChanged()
-            .bind(with: self) { owner, value in
-                print("실시간 검색: \(value)")
-                let result = value.isEmpty ? owner.data : owner.data.filter { $0.contains(value) }
-                owner.list.accept(result)
-            }
-            .disposed(by: disposeBag)
-        
-        navigationItem.rightBarButtonItem?.rx.tap
-            .withLatestFrom(searchBar.rx.text.orEmpty) { void, text in
-                return text
-            }
-            .bind(with: self) { owner, value in
-                print("추가: \(value)")
-                owner.data.insert(value, at: 0)
-                owner.list.accept(owner.data)
-            }
-            .disposed(by: disposeBag)
     }
     
     private func setSearchController() {
         view.addSubview(searchBar)
         navigationItem.titleView = searchBar
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(plusButtonClicked))
-    }
-    
-    @objc func plusButtonClicked() {
-        print("추가 버튼 클릭")
+        let plusButton = UIBarButtonItem(title: "추가")
+        navigationItem.rightBarButtonItem = plusButton
     }
     
     private func configure() {
---
+++ SeSACRxThreads/Scenes/Search/SearchViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  SearchViewModel.swift
+//  SeSACRxThreads
+//
+//  Created by 김성민 on 8/5/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class SearchViewModel {
+    
+    var data = ["A", "B", "C", "AB", "D", "ABC", "BBB", "EC", "SA", "AAAB", "ED", "F", "G", "H"]
+    
+    let disposeBag = DisposeBag()
+    
+    struct Input {
+        let searchText: ControlProperty<String>
+        let barButtonTap: ControlEvent<Void>?
+    }
+    
+    struct Output {
+        let list: BehaviorRelay<[String]>
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        let list = BehaviorRelay<[String]>(value: data)
+        
+        input.searchText
+            .debounce(.seconds(1), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .bind(with: self) { owner, value in
+                print("실시간 검색: \(value)")
+                let result = value.isEmpty ? owner.data : owner.data.filter { $0.contains(value) }
+                list.accept(result)
+            }
+            .disposed(by: disposeBag)
+        
+        input.barButtonTap?
+            .withLatestFrom(input.searchText) { void, text in
+                return text
+            }
+            .bind(with: self) { owner, value in
+                print("추가: \(value)")
+                owner.data.insert(value, at: 0)
+                list.accept(owner.data)
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(list: list)
+    }
+}
